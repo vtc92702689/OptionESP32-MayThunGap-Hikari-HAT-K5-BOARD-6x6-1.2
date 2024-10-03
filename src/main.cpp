bf34789084492fded48858a6b94a565c00663f77
vtc92702689 <91804219+vtc92702689@users.noreply.github.com>
2024-10-03 19:29:48 +0700

    10/3

--- src/main.cpp
+++ src/main.cpp
@@ -3,73 +3,271 @@
 #include <U8g2lib.h>
 #include <Preferences.h>
 #include <ArduinoJson.h>
+#include <OneButton.h>
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // Khởi tạo đối tượng màn hình OLED U8G2
+
+const char* jsonString = "{\
+  \"main\": {\
+    \"main1\": {\
+      \"text\": \"Cài Đặt\",\
+      \"key\": \"CD\",\
+      \"children\": {\
+        \"CD1\": {\
+          \"text\": \"Text1\",\
+          \"value\": \"GT1\",\
+          \"function\": \"CN1\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD2\": {\
+          \"text\": \"Text2\",\
+          \"value\": \"GT2\",\
+          \"function\": \"CN2\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD3\": {\
+          \"text\": \"Text3\",\
+          \"value\": \"GT3\",\
+          \"function\": \"CN3\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD4\": {\
+          \"text\": \"Text4\",\
+          \"value\": \"GT4\",\
+          \"function\": \"CN4\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD5\": {\
+          \"text\": \"Text5\",\
+          \"value\": \"GT5\",\
+          \"function\": \"CN5\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD6\": {\
+          \"text\": \"Text6\",\
+          \"value\": \"GT6\",\
+          \"function\": \"CN6\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD7\": {\
+          \"text\": \"Text7\",\
+          \"value\": \"GT7\",\
+          \"function\": \"CN7\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD8\": {\
+          \"text\": \"Text8\",\
+          \"value\": \"GT8\",\
+          \"function\": \"CN8\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD9\": {\
+          \"text\": \"Text9\",\
+          \"value\": \"GT9\",\
+          \"function\": \"CN9\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        },\
+        \"CD10\": {\
+          \"text\": \"Text10\",\
+          \"value\": \"GT10\",\
+          \"function\": \"CN10\",\
+          \"belonging\": \"\",\
+          \"minValue\": 1,\
+          \"maxValue\": 1000,\
+          \"accessAllowed\": true,\
+          \"editAllowed\": true\
+        }\
+      }\
+    },\
+    \"main2\": {\
+      \"text\": \"Kiểm Tra\",\
+      \"key\": \"KT\",\
+      \"children\": {}\
+    },\
+    \"main3\": {\
+      \"text\": \"Giới Thiệu\",\
+      \"key\": \"GT\",\
+      \"children\": {}\
+    }\
+  }\
+}";
 
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-// put function declarations here:
-void wrapText(const char* text, int16_t x, int16_t y, int16_t lineHeight, int16_t maxWidth) {
-  int16_t cursorX = x;
-  int16_t cursorY = y;
-  const char* wordStart = text;
-  const char* currentChar = text;
+// Khai báo các nút
+/*const int btnMenu = 32;
+const int btnSet = 33;
+const int btnUp = 34;
+const int btnDown = 35;*/
 
-  while (*currentChar) {
-    // Di chuyển đến cuối từ hiện tại
-    if (*currentChar == ' ' || *(currentChar + 1) == '\0') {
-      // Tạo một chuỗi tạm cho từ
-      char word[64];
+OneButton btnMenu(32, false);
+OneButton btnSet(33, false);
+OneButton btnUp(34, false);
+OneButton btnDown(35, false);
+
+void wrapText(const char* text, int16_t x, int16_t y, int16_t lineHeight, int16_t maxWidth) {   // Hàm wrapText để hiển thị văn bản xuống dòng nếu dài quá
+  int16_t cursorX = x;  // Vị trí x bắt đầu in
+  int16_t cursorY = y;  // Vị trí y bắt đầu in
+  const char* wordStart = text;  // Vị trí bắt đầu của từ trong chuỗi
+  const char* currentChar = text;  // Ký tự hiện tại đang xử lý
+
+  while (*currentChar) {     // Vòng lặp qua từng ký tự trong chuỗi
+    if (*currentChar == ' ' || *(currentChar + 1) == '\0') {    
+      char word[64];   // Tạo chuỗi tạm để chứa từ hiện tại
       int len = currentChar - wordStart + 1;
       strncpy(word, wordStart, len);
       word[len] = '\0';
 
-      // Kiểm tra xem từ có vừa với chiều rộng còn lại không
-      int16_t textWidth = u8g2.getStrWidth(word);
+      int16_t textWidth = u8g2.getStrWidth(word);  // Kiểm tra nếu từ có vừa với chiều rộng màn hình
       if (cursorX + textWidth > maxWidth) {
-        // Xuống dòng nếu từ không vừa
-        cursorX = x;
-        cursorY += lineHeight;
+        cursorX = x;  // Nếu từ quá dài, xuống dòng
+        cursorY += lineHeight;  // Tăng vị trí y để xuống dòng
       }
 
-      // In từ lên màn hình
-      u8g2.drawStr(cursorX, cursorY, word);
-      cursorX += textWidth;
+      u8g2.drawStr(cursorX, cursorY, word);  // Vẽ từ lên màn hình
+      cursorX += textWidth;  // Cập nhật vị trí x cho từ tiếp theo
       
-      // Nếu là ký tự khoảng trắng, in khoảng trắng
       if (*currentChar == ' ') {
-        cursorX += u8g2.getStrWidth(" ");
+        cursorX += u8g2.getStrWidth(" ");  // Thêm khoảng trắng nếu ký tự là ' '
       }
 
-      // Chuyển đến từ tiếp theo
-      wordStart = currentChar + 1;
+      wordStart = currentChar + 1; // Di chuyển đến từ tiếp theo
     }
-    currentChar++;
+    currentChar++;  // Chuyển ký tự hiện tại sang ký tự tiếp theo
   }
 }
 
-void showSetup(const char* setUpCode, const char* value, const char* text) {
-  u8g2.begin();
-	u8g2.clearBuffer();
-	u8g2.setFont(u8g2_font_crox3hb_tf);
-	u8g2.drawStr(0, 18, strcat(setUpCode,";"));  //   Hiển thị mã cài đặt
-  u8g2.setFont(u8g2_font_crox3h_tf);  //font không đậm
-  u8g2.drawStr(82, 18, value);  //  Hiển thị thông số cài đặt
-	u8g2.drawLine(0, 23, 128, 23); // Vẽ một line thẳng
 
-	wrapText(text, 0, 46, 18, 128);   // Tọa độ bắt đầu (0, 15), chiều cao mỗi dòng (12), chiều rộng tối đa (128)
+void showSetup(const char* setUpCode, const char* value, const char* text) {   // Hàm hiển thị màn hình với thông tin mã cài đặt và giá trị
+  u8g2.clearBuffer();  // Xóa bộ nhớ đệm của màn hình để vẽ mới
+  u8g2.setFont(u8g2_font_crox3hb_tf);  // Thiết lập font chữ đậm
 
-  u8g2.sendBuffer();
+  char tempSetUpCode[64];    // Tạo một chuỗi tạm chứa mã cài đặt và dấu ";"
+  snprintf(tempSetUpCode, sizeof(tempSetUpCode), "%s;", setUpCode);  // Nối mã cài đặt với dấu ";"
+  
+  u8g2.drawStr(0, 18, tempSetUpCode);  // Hiển thị mã cài đặt (tại vị trí x=0, y=18)
+  u8g2.setFont(u8g2_font_crox3h_tf);  // Thiết lập font chữ thường (không đậm)
+  u8g2.drawStr(82, 18, value);  // Hiển thị giá trị cài đặt (tại vị trí x=82, y=18)
+  u8g2.drawLine(0, 23, 128, 23);  // Vẽ một đường ngang trên màn hình (tọa độ từ x=0 đến x=128)
+
+  wrapText(text, 0, 42, 18, 128);  // Bắt đầu tại tọa độ x=0, y=46, mỗi dòng cách nhau 18 điểm, tối đa chiều rộng 128 điểm
+  u8g2.sendBuffer(); // Gửi nội dung đệm ra màn hình
+}
+
+void btnMenuClick() {
+  Serial.println("Button Clicked (nhấn nhả)");
+}
+
+// Hàm callback khi bắt đầu nhấn giữ nút
+void btnMenuLongPressStart() {
+  Serial.println("Button Long Press Started (bắt đầu nhấn giữ)");
+}
+
+// Hàm callback khi nút đang được giữ
+void btnMenuDuringLongPress() {
+  Serial.println("Button is being Long Pressed (đang giữ)");
+}
+
+void btnSetClick() {
+  showSetup("SET", "OFF", "da lick");
+}
+
+// Hàm callback khi bắt đầu nhấn giữ nút
+void btnSetLongPressStart() {
+  showSetup("Setup", "OFF", "BatDauNhanGiu");
+}
+
+// Hàm callback khi nút đang được giữ
+void btnSetDuringLongPress() {
+  showSetup("Setup", "OFF", "Dang giu nut");
+}
+
+void btnUpClick() {
+  Serial.println("Button Clicked (nhấn nhả)");
+}
+
+// Hàm callback khi bắt đầu nhấn giữ nút
+void btnUpLongPressStart() {
+  Serial.println("Button Long Press Started (bắt đầu nhấn giữ)");
+}
+
+// Hàm callback khi nút đang được giữ
+void btnUpDuringLongPress() {
+  Serial.println("Button is being Long Pressed (đang giữ)");
+}
+
+void btnDownClick() {
+  Serial.println("Button Clicked (nhấn nhả)");
+}
+
+// Hàm callback khi bắt đầu nhấn giữ nút
+void btnDownLongPressStart() {
+  Serial.println("Button Long Press Started (bắt đầu nhấn giữ)");
+}
+
+// Hàm callback khi nút đang được giữ
+void btnDownDuringLongPress() {
+  Serial.println("Button is being Long Pressed (đang giữ)");
 }
 
 void setup() {
-  // Khởi tạo màn hình
-  Serial.begin(115200);
-  u8g2.begin();
+
+  Serial.begin(115200);     // Khởi tạo Serial và màn hình
+  u8g2.begin();  // Khởi tạo màn hình OLED
+  StaticJsonDocument<200> doc;
+
+  DeserializationError error = deserializeJson(doc, jsonString);    // Phân tích chuỗi JSON
+  if (error) {
+    showSetup("Error","E002","Json Error");
+    /*Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;*/
+  }
+
+  btnMenu.attachClick(btnMenuClick);
+  btnMenu.attachLongPressStart(btnMenuLongPressStart);
+  btnMenu.attachDuringLongPress(btnMenuDuringLongPress);
+
+  btnSet.attachClick(btnSetClick);
+  btnSet.attachLongPressStart(btnSetLongPressStart);
+  btnSet.attachDuringLongPress(btnSetDuringLongPress);
 }
 
 void loop() {
-  showSetup("U99", "9998", "Cai dat nay de test");
-  delay(2000);
-  showSetup("U97", "9997", "Cai dat nay de test s2");
-  delay(2000);
-  showSetup("U96", "9996", "Cai dat nay de test s3");
+  btnSet.tick();
 }
